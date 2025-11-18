8b88173878eff23f0ef63731fd25c92d8b67b9b2
taseeruaeinfo <taseeruaeinfo@gmail.com>
2025-11-18 14:39:00 +0400

    Update BookCall.jsx

--- src/pages/BookCall.jsx
+++ src/pages/BookCall.jsx
@@ -8,6 +8,11 @@ export default function BookCall() {
     script.src = "https://assets.calendly.com/assets/external/widget.js";
     script.async = true;
     document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+    
   }, []);
 
   return (
