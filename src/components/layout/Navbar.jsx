53452262507a300d99650db5b2ac5b74160a3b6f
taseeruaeinfo <taseeruaeinfo@gmail.com>
2025-11-18 14:46:46 +0400

    Update Navbar.jsx

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -139,12 +139,12 @@ const Navbar = () => {
                             </Link>
                         </motion.div>
                         <motion.div whileHover={{ scale: 1.05 }}>
-                    {/*     <a href="https://calendly.com/nehajakhar401/30min" target="_blank" rel="noopener noreferrer" className="btn btn-primary">
+                            <a href="https://calendly.com/nehajakhar401/30min" target="_blank" rel="noopener noreferrer" className="btn btn-primary">
                                 Book a Call
-                            </a> */}
-                            <Link to="/book-call" className="btn btn-outline">
+                            </a> 
+                            {/* <Link to="/book-call" className="btn btn-outline">
                                 Book a Call
-                            </Link>
+                            </Link> */}
                         </motion.div>
                     </motion.div>
 
@@ -248,12 +248,12 @@ const Navbar = () => {
                                         </Link>
                                     </motion.div>
                                     <motion.div whileTap={{ scale: 0.97 }}>
-                                        {/*  <a href="https://calendly.com/nxtstar" target="_blank" rel="noopener noreferrer" className="btn btn-primary">
+                                        <a href="https://calendly.com/nxtstar" target="_blank" rel="noopener noreferrer" className="btn btn-primary">
                                             Book a Call
-                                        </a> */}                                        
-                                        <Link to="/book-call" className="btn btn-outline">
+                                        </a>                                      
+                                        {/* <Link to="/book-call" className="btn btn-outline">
                                             Book a Call
-                                        </Link>
+                                        </Link> */}
                                     </motion.div>
                                 </motion.div>
                             </motion.div>
