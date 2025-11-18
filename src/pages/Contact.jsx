dddcd9c448985182465b7f9d5fc310afe1eb14bc
taseeruaeinfo <taseeruaeinfo@gmail.com>
2025-11-18 15:31:30 +0400

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -118,7 +118,7 @@ const Contact = () => {
                                 <FaEnvelope />
                             </div>
                             <h3>Email Us</h3>
-                            <p>nxtstar.business.setup@gmail.com</p>
+                            <p>nehajakhar@nxtstar.ae</p>
                         </div>
 
                         <div className="contact-card">
