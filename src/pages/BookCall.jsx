479dca6df9e23197b7d9174b75ea7cf73fbc80b4
taseeruaeinfo <taseeruaeinfo@gmail.com>
2025-11-18 14:30:06 +0400

    Update BookCall.jsx

--- src/pages/BookCall.jsx
+++ src/pages/BookCall.jsx
@@ -1,4 +1,34 @@
-<!-- Calendly inline widget begin -->
-<div class="calendly-inline-widget" data-url="https://calendly.com/nehajakhar-nxtstar" style="min-width:320px;height:700px;"></div>
-<script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
-<!-- Calendly inline widget end -->
+import { useEffect } from "react";
+
+export default function BookCall() {
+
+  useEffect(() => {
+    // Load Calendly script dynamically
+    const script = document.createElement("script");
+    script.src = "https://assets.calendly.com/assets/external/widget.js";
+    script.async = true;
+    document.body.appendChild(script);
+  }, []);
+
+  return (
+    <!-- Calendly inline widget begin -->
+      <div
+        class="calendly-inline-widget"
+        data-url="https://calendly.com/nehajakhar-nxtstar"
+        style="min-width:320px;height:700px;"
+        ></div>
+      {/*   <script 
+        type="text/javascript" 
+        src="https://assets.calendly.com/assets/external/widget.js" 
+        async
+        ></script>
+  {/* <div
+      className="calendly-inline-widget"
+      data-url="https://calendly.com/nehajakhar401/30min"
+      style={{ minWidth: "320px", height: "700px" }}
+    ></div> */}
+
+    
+  
+  );
+}
